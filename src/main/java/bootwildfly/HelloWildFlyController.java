73433973bd53315dcc0d60380f33973cf90188c3
Piotr Kotynia <P.KOTYNIA@oberthur.com>
2017-10-01 11:46:42 +0200

    kotlin roll back

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,10 +1,7 @@
 package bootwildfly;
 
-import hello.KotlinHelloKt;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static hello.JavaHello.getHelloStringFromKotlin;
 
 @RestController
 public class HelloWildFlyController {
@@ -12,7 +9,6 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
-
-        return "hello " + getHelloStringFromKotlin() + KotlinHelloKt.getHelloStringFromJava();
+        return ("Hello, SpringBoot on Wildfly");
     }
 }
