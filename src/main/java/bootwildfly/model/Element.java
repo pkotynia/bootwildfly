bdb312cc39da16582a221a74b667a566b86f308b
Piotr Kotynia <P.KOTYNIA@oberthur.com>
2017-10-08 10:21:17 +0200

    model updated

---
+++ src/main/java/bootwildfly/model/Element.java
@@ -0,0 +1,23 @@
+package bootwildfly.model;
+
+public class Element {
+
+    private String content;
+    private Boolean visible;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+}
---
+++ src/main/java/bootwildfly/model/Question.java
@@ -0,0 +1,33 @@
+package bootwildfly.model;
+
+public class Question {
+
+    private Element[] elements;
+    private String[] suggestions;
+    private String answer;
+
+    public Element[] getElements() {
+        return elements;
+    }
+
+    public void setElements(Element[] elements) {
+        this.elements = elements;
+    }
+
+    public String[] getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(String[] suggestions) {
+        this.suggestions = suggestions;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
